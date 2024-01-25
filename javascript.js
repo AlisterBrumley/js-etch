af5859dcdaaa35d8fc3bb3e1a94149b0e0652eba
Alister Brumley <109202180+AstaBrum@users.noreply.github.com>
2024-01-26 06:53:50 +1100

    basic grid drawing
    currently drawing a grid which is hardcoded, need to clean/speed up

--- javascript.js
+++ javascript.js
@@ -2,3 +2,35 @@ const gridWidth = 16;
 const gridSize = gridWidth ** 2;
 
 console.log(gridSize);
+
+function drawGrid(gWidth, gSize) {
+    const gridContainer = document.querySelector(".gridContainer");
+    const gridFragment = new DocumentFragment();
+
+    // creating initial row
+    let gridRow = document.createElement("div");
+    gridRow.classList.add("gridRow")
+
+    // creating grid
+    for (let addBlock = 1; addBlock <= gSize; addBlock++) {
+        // creating blocks
+        const gridBlock = document.createElement("div");
+        gridBlock.classList.add("block");
+        gridRow.appendChild(gridBlock);
+
+        // if at row end, append to container and create new row
+        if (addBlock % gWidth === 0) {
+            gridContainer.appendChild(gridRow);
+            gridRow = document.createElement("div");
+            gridRow.classList.add("gridRow")
+        }
+    };
+    
+    // TODO: use fragments to speed up
+    // Currently kinda breaky at 100
+}
+
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    drawGrid(gridWidth, gridSize);
+});
